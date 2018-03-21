b23fc3deedf1aebfe816e5a58f68233382497b4a
skerschb <sue.kerschbaumer@10gen.com>
2018-03-21 12:11:46 -0400

    generator reformat

--- src/com/mongodb/docs/dataset/generator/DataSetGenerator.java
+++ src/com/mongodb/docs/dataset/generator/DataSetGenerator.java
@@ -9,39 +9,41 @@ import com.mongodb.docs.dataset.objects.Product;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 
-
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 import java.util.List;
 
-
 public class DataSetGenerator {
 
-	public static void main(String args[]) {
-		
-  		final MongoClientURI uri = new MongoClientURI("mongodb://testuser:password@localhost:27017/test?authSource=admin");
-		
-  		final MongoClient mongoClient = new MongoClient(uri);
-		
-		MongoDatabase mongoDB = mongoClient.getDatabase("test");
-			
-		CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
-                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-		
-		mongoDB = mongoDB.withCodecRegistry(pojoCodecRegistry);
-		
-		MongoCollection<Product> collection = mongoDB.getCollection("inventory", Product.class);
-		
-		collection.drop();
-	    
+    public static void main(String args[]) {
+
+        final MongoClientURI uri = new MongoClientURI(
+                "mongodb://testuser:password@localhost:27017/test?authSource=admin");
+
+        final MongoClient mongoClient = new MongoClient(uri);
+
+        MongoDatabase mongoDB = mongoClient.getDatabase("test");
+
+        CodecRegistry pojoCodecRegistry = fromRegistries(
+                MongoClient.getDefaultCodecRegistry(),
+                fromProviders(PojoCodecProvider.builder()
+                        .automatic(true).build()));
+
+        mongoDB = mongoDB.withCodecRegistry(pojoCodecRegistry);
+
+        MongoCollection<Product> collection = mongoDB
+                .getCollection("inventory", Product.class);
+
+        collection.drop();
+
         List<Product> products = ProductUtil.generateProducts();
-        
+
         if (null != products)
             collection.insertMany(products);
-				
-		mongoClient.close();
-		
-	}
+
+        mongoClient.close();
+
+    }
 
 }
